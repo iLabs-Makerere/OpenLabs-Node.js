29152ef475e0546c9969a7398ef00c62d611deed
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2017-03-24 15:54:56 +0300

    undefined resave option corrected

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'secret',
     //saveUnitialized: true,
-    //resave: true
+    saveUninitialized: true,
+    resave: true
 }));
 
 //passort init
