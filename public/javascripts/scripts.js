e6e800ecc920e7424172e4d753edb046b38ea6b3
Lornah Lynn Tumuhairwe <ltumuhairwe@tusdavid.com>
2017-04-24 20:19:46 +0300

    integrated graphing with smoothie

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,7 +3,15 @@ jQuery(document).ready(function($) {
 	/*if($('#plotly-div').length){ //refer to graphing.js
         graphingFxn();
   	}*/
+		var channelA = new TimeSeries();
+  	var channelB = new TimeSeries();
     createTimeline();
+		var socket = new WebSocket( 'ws://107.170.249.214:3131' );
+	
+		socket.onopen = function(){
+					var msg = JSON.stringify(["OpenLabsHost","Connect"]);
+					socket.send(msg);
+			}
     $('#wind-speed').hide();
     $("#stoplabBtn").prop('disabled', true);
 
@@ -18,11 +26,31 @@ jQuery(document).ready(function($) {
         $("#stoplabBtn").prop('disabled', true);
     });
 	//console.log(this.location.pathname);
+	socket.onmessage = function(msg){
+		
+		var positionA = msg.data.indexOf('A');
+		var positionB = msg.data.indexOf('B');
+		var positionC = msg.data.indexOf('C');
+		var valueA = msg.data.substring(positionA + 1, positionB);
+		var valueB = msg.data.substring(positionB + 1, positionC);
+		var valueC = msg.data.substr(positionC + 1);
+		valueA -= valueC;
+		valueB -= valueC;
+		console.log(msg);
+		//$(".voltage").html(valueB);
+		channelA.append(new Date().getTime(), parseFloat(valueA));
+		channelB.append(new Date().getTime(), parseFloat(valueB));
+	}
+	
 	$('#main-navbar .nav a[href="' + this.location.pathname + '"]').parent().addClass('active');
 
     function createTimeline() {
-        var chart = new SmoothieChart();
-        chart.streamTo(document.getElementById("mycanvas"), 500);
+        //var chart = new SmoothieChart();
+				var chart = new SmoothieChart({minValue: -500.00, maxValue: 500.00,interpolation:'step',
+																			grid: {sharpLines:true, lineWidth: 1, millisPerLine: 1000, verticalSections: 6}});
+				chart.addTimeSeries(channelA, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0)', lineWidth: 2 });
+				chart.addTimeSeries(channelB, { strokeStyle: 'rgba(240, 0, 0, 1)', fillStyle: 'rgba(240, 0, 0, 0)', lineWidth: 2 });
+        chart.streamTo(document.getElementById("mycanvas"), 1000);
     }
 });
 
---
+++ public/javascripts/server.js
@@ -0,0 +1,38 @@
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 3131}); //ws node library
+
+var ws_clients = {}
+
+// wss.on('connection', function(ws) {
+//     ws.on('message', function(message) {
+//         console.log('received: %s', message);
+//     });
+//     ws.send('something');
+// });  //documentation
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log('R: %s', message);
+        var data = JSON.parse(message); //JSON.parse turns a string of JSON text into a Javascript object.
+        action = data[1];
+        if(action == "Connect"){
+            var client = data[0];
+            ws_clients[client] = ws;//saves client to ws_clients object
+        }else{
+            var destination = data[0];
+            try{
+                ws_clients[destination].send(data[1]);
+            }catch(e){
+            }
+            //wss.broadcast(message);
+        }
+    });
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        try{
+            client.send(data);
+        }catch(e){
+        }
+    });
+};
