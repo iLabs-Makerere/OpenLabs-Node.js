1f26adaf0b1adf5a85cc4d63544e061659aaa8d6
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-05-09 14:41:45 +0300

    updated client code

--- serial/client.py
+++ serial/client.py
@@ -5,8 +5,11 @@ import serial
 import thread
 import time
 
+prevTime = 0
+
 def on_message(ws, message):
-    connection.write(""+message+"".encode())
+    connection.write(message.encode())
+    #connection.write(b+message)
     print message
 
 def on_error(ws, error):
@@ -24,19 +27,49 @@ def on_open(ws):
         # data_log = connection.readline().decode('ascii')
         # print data_log
     def run(*args):
+        valuePair = [None,None]
         while True:
-            data_log = connection.readline()
-            time.sleep(0.1)
-            print data_log
-            data_rw = ["OpenLabsHost",data_log]
-            data_js = json.dumps(data_rw)
-            ws.send(data_js)
-            pass
+            data_log = connection.readline().strip("\r\n")
+            print (data_log)
+            try:
+                data_val = int(data_log[1:4])
+                if valuePair[0] is None:
+                    valuePair[0] = data_val
+                    send_data(ws, data_log)
+                elif valuePair[0] > 500:
+                    if valuePair[0] == 1000 and data_val > 500:
+                        valuePair[0] = data_val
+                        send_data(ws, data_log)
+                    elif abs(valuePair[0] - data_val) <= 20:
+                        valuePair[0] = 0
+                        send_data(ws, data_log)
+                else:
+                    if valuePair[0] == 0 and data_val < 500:
+                        valuePair[0] = data_val
+                        send_data(ws, data_log)
+                    elif abs(valuePair[0] - data_val) <= 20:
+                        valuePair[0] = 1000
+                        send_data(ws, data_log)
+            except ValueError:
+                pass
     thread.start_new_thread(run, ())
 
+    def send_data(ws, data_log):
+        global prevTime
+        current_time = time.time() * 1000
+        if prevTime is 0:
+            time_delay = 0
+        else:
+            time_delay = current_time - prevTime
+        prevTime = current_time
+        data_log += "T" + str(time_delay)
+        data_rw = ["OpenLabsHost", data_log]
+        data_js = json.dumps(data_rw)
+        ws.send(data_js)
+        time.sleep(0.5)
 
 if __name__ == "__main__":
-    connection = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout = 0.5)
+    connection = serial.Serial('/dev/ttyUSB1', baudrate=115200, timeout = 0.5)
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(
         "ws://107.170.249.214:3131",
