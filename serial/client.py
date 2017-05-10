a5239d18bc961815ea6cc2f9f5693f4311b89a8b
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2017-05-10 09:48:02 +0300

    update in client

--- serial/client.py
+++ serial/client.py
@@ -4,13 +4,18 @@ import json
 import serial
 import thread
 import time
+import json
 
 prevTime = 0
+interpolation = 'step'
 
 def on_message(ws, message):
-    connection.write(message.encode())
-    #connection.write(b+message)
-    print message
+    global interpolation
+    message = json.loads(message)
+    connection.write(message[0].encode())
+    interpolation = message[1]
+    print message[0]
+    print message[1]
 
 def on_error(ws, error):
     print error
@@ -32,24 +37,49 @@ def on_open(ws):
             data_log = connection.readline().strip("\r\n")
             print (data_log)
             try:
-                data_val = int(data_log[1:4])
-                if valuePair[0] is None:
-                    valuePair[0] = data_val
-                    send_data(ws, data_log)
-                elif valuePair[0] > 500:
-                    if valuePair[0] == 1000 and data_val > 500:
+                if interpolation == 'step':
+                    data_val = int(data_log[1:4])
+                    if valuePair[0] is None:
                         valuePair[0] = data_val
                         send_data(ws, data_log)
-                    elif abs(valuePair[0] - data_val) <= 20:
-                        valuePair[0] = 0
-                        send_data(ws, data_log)
-                else:
-                    if valuePair[0] == 0 and data_val < 500:
+                    elif valuePair[0] > 500:
+                        if valuePair[0] == 1000 and data_val > 500:
+                            valuePair[0] = data_val
+                            send_data(ws, data_log)
+                        elif abs(valuePair[0] - data_val) <= 20:
+                            valuePair[0] = 0
+                            send_data(ws, data_log)
+                    else:
+                        if valuePair[0] == 0 and data_val < 500:
+                            valuePair[0] = data_val
+                            send_data(ws, data_log)
+                        elif abs(valuePair[0] - data_val) <= 20:
+                            valuePair[0] = 1000
+                            send_data(ws, data_log)
+                if interpolation == 'bezier':
+                    data_val = int(data_log[1:4])
+                    if valuePair[0] is None:
                         valuePair[0] = data_val
                         send_data(ws, data_log)
-                    elif abs(valuePair[0] - data_val) <= 20:
-                        valuePair[0] = 1000
+                    elif valuePair[0] > 500 and data_val < 500:
+                        #if valuePair[0] < 1000 and data_val > 500:
+                        valuePair[0] = data_val
                         send_data(ws, data_log)
+                    elif valuePair[0] < 500 and data_val > 500:
+                        valuePair[0] = data_val
+                        send_data(ws, data_log)
+                    else:
+                        pass
+                    #     elif abs(valuePair[0] - data_val) <= 10:
+                    #         valuePair[0] = 0
+                    #         send_data(ws, data_log)
+                    # else:
+                    #     if valuePair[0] == 0 and data_val < 500:
+                    #         valuePair[0] = data_val
+                    #         send_data(ws, data_log)
+                    #     elif abs(valuePair[0] - data_val) <= 20:
+                    #         valuePair[0] = 1000
+                    #         send_data(ws, data_log)
             except ValueError:
                 pass
     thread.start_new_thread(run, ())
@@ -69,7 +99,7 @@ def on_open(ws):
         time.sleep(0.5)
 
 if __name__ == "__main__":
-    connection = serial.Serial('/dev/ttyUSB1', baudrate=115200, timeout = 0.5)
+    connection = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout = 0.5)
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(
         "ws://107.170.249.214:3131",
